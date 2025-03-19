6ffe0f782d56384b41b0a7a1376077e2b8ad7e64
Vladimir-Iluk <nezalejniy@ukr.net>
2025-03-19 10:19:28 +0200

    Complete Interface segregation

---
+++ src/ua/edu/chnu/kkn/solid_violation/isp/AnimalFly.java
@@ -0,0 +1,5 @@
+package ua.edu.chnu.kkn.solid_violation.isp;
+
+public interface AnimalFly {
+    void fly();
+}
--- src/ua/edu/chnu/kkn/solid_violation/isp/Bird.java
+++ src/ua/edu/chnu/kkn/solid_violation/isp/Bird.java
@@ -1,10 +1,11 @@
 package ua.edu.chnu.kkn.solid_violation.isp;
 
-public class Bird implements Animal {
-    public void bark() { }
+public class Bird implements AnimalFly,AnimalRun {
+    @Override
     public void run() {
         System.out.print("Bird is running");
     }
+    @Override
     public void fly() {
         System.out.print("Bird is flying");
     }
